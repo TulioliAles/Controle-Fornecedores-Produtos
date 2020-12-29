627f653bea5ec0bd648394a57ce513a2a02ff04e
TulioliAles <desmecad@hotmail.com>
2020-12-28 22:15:58 -0300

    Criados o DBContext, os Mapeamentos das entidades e geradas as Migrations

---
+++ src/DevIO.Data/Context/CFPDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class CFPDbContext : DbContext
+    {
+        public CFPDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CFPDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
